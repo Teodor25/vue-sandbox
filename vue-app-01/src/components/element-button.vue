79ec650b5639d97140d620a67e7978c9828a3f87
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-03-11 12:00:37 +0100

    feat: 🎸 Initialize app with layout

---
+++ vue-app-01/src/components/element-button.vue
@@ -0,0 +1,23 @@
+<template>
+    <button :style="{ background: color }" class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: "element-button",
+    props: {
+        text: {
+            type: String,
+            default: 'no text'
+        },
+        color: {
+            type: String,
+            default: "lightblue"
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-app-01/src/components/layout-header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button :text="'Add task'" color="green" />
+    </header>
+</template>
+
+<script>
+import Button from './element-button.vue'
+
+export default {
+    name: 'layout-header',
+    props: {
+        title: String,
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+        margin-bottom: 20px;
+    }
+</style>
