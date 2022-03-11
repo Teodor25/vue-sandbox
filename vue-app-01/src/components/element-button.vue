afa09ee3cf24e0c06c56f72031091bd7bfa893b9
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-03-11 12:02:22 +0100

    feat: 🎸 Add button click functionality

--- vue-app-01/src/components/element-button.vue
+++ vue-app-01/src/components/element-button.vue
@@ -1,5 +1,11 @@
 <template>
-    <button :style="{ background: color }" class="btn">{{text}}</button>
+    <button 
+        :style="{ background: color }" 
+        class="btn" 
+        @click="onClick()"
+    >
+        {{text}}
+    </button>
 </template>
 
 <script>
@@ -13,6 +19,11 @@ export default {
         color: {
             type: String,
             default: "lightblue"
+        }
+    },
+    methods: {
+        onClick() {
+            alert('Click');
         }
     }
 }
